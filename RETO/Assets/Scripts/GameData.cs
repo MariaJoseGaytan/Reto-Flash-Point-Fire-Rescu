d4153501f22b20a8539a1017ec289f52e9420694
MariaJoseGaytan <A01706616@tec.mx>
2024-11-25 19:10:14 -0600

    piso

--- RETO/Assets/Scripts/GameData.cs
+++ RETO/Assets/Scripts/GameData.cs
@@ -1,21 +1,21 @@
+using UnityEngine;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
-// Clase que representa una celda en el tablero
 public class CellState
 {
-    public List<int> cell_position; // [x, y]
-    public List<AgentData> agents; // Lista de agentes en la celda
-}
-
-// Clase que representa un agente en el tablero
-public class AgentData
-{
-    public string type; // CellAgent, MarkerAgent, FireMarkerAgent, etc.
-    public string unique_id; // ID único del agente
-    public List<int> position; // [x, y]
-    public string walls; // Para CellAgent, cadena de 4 dígitos indicando paredes
-    public string marker_type; // Para MarkerAgent: "v" o "f"
-    public List<int> connected_cell; // Para DoorAgent: posición de la celda conectada
-     public bool is_entrance;         // Indica si la celda es una entrada (true) o no (false)
+    public string alert;         
+    public List<List<int>> door; 
+    public bool down;            
+    public bool entrance;        
+    public string fire;          
     
+    [JsonProperty("is agent")]
+    public string is_agent;      
+    
+    public bool left;            
+    public List<int> pos;        
+    public bool right;           
+    public string step;          
+    public bool up;              
 }
--- RETO/Assets/Scripts/ServerManager.cs
+++ RETO/Assets/Scripts/ServerManager.cs
@@ -4,11 +4,11 @@ using System.Collections;
 
 public class ServerManager : MonoBehaviour
 {
-    private string url = "http://127.0.0.1:5000/state"; // URL del servidor tablero
+    private string url = "http://127.0.0.1:5000/"; // URL del servidor tablero
 
     public IEnumerator GetGameState(System.Action<string> callback)
 {
-    UnityWebRequest www = UnityWebRequest.Get("http://127.0.0.1:5000/state");
+    UnityWebRequest www = UnityWebRequest.Get("http://127.0.0.1:5000/");
     yield return www.SendWebRequest();
 
     if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
